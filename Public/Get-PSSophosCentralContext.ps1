72ee7d93a22286d40f767793cb86d2ff0b787733
adrianm <adrian.muscat@systech.net>
2024-07-08 11:53:31 +0200

    Corrected output type

--- Public/Get-PSSophosCentralContext.ps1
+++ Public/Get-PSSophosCentralContext.ps1
@@ -1,29 +1,29 @@
-function Get-SophosCentralContext {
+function Get-PSSophosCentralContext {
     <#
     .SYNOPSIS
-    Gets the metadata usedto authenticate HTTPS requests
-    
+    Gets the metadata used to authenticate HTTPS requests
+
     .DESCRIPTION
     Get's the Region and TenantID which is used in the HTTPS request headers
-    
+
     .EXAMPLE
     $response = Connect-SophosCentral -clientid $clientid -clientsecret $clientsecret
-    Get-SophosCentralContext -token $response.access_token
+    Get-PSSophosCentralContext -token $response.access_token
 
     .NOTES
     General notes
     #>
     [CmdletBinding()]
-    [OutputType([Object])]
+    [OutputType([PSCustomObject])]
     param (
         # Token to be used in the header of the HTTPS request
         [Parameter(Mandatory = $true)]
         [string]
         $token
     )
-    
+
     process {
-        
+
         $headers = @{"Authorization" = "Bearer $token" }
         $gdrresponse = Invoke-RestMethod 'https://api.central.sophos.com/whoami/v1' -Method 'GET' -Headers $headers
         $TenantId = $gdrresponse.id
@@ -36,5 +36,5 @@ function Get-SophosCentralContext {
         }
 
     } #process
-    
+
 } #Get-SophosCentralContext function
