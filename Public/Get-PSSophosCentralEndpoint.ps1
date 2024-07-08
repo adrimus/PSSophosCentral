62c772300e5881456e452199f3835c760928ee51
adrianm <adrian.muscat@systech.net>
2024-07-08 11:53:56 +0200

    Added script varibles

--- Public/Get-PSSophosCentralEndpoint.ps1
+++ Public/Get-PSSophosCentralEndpoint.ps1
@@ -2,14 +2,15 @@ function Get-PSSophosCentralEndpoint {
     <#
     .SYNOPSIS
     Get's info an a endpoint registered in Sophos Central
-    
+
     .DESCRIPTION
     Uses the endpoint API to get information on an endpoint based on ID. This command needs the tenant ID and JWT for the header information.
-    
+
     .EXAMPLE
     Get-PSSophosCentralEndpoint
-    
+
     .NOTES
+    The Data Region and Tenant ID should be available in the Script scope from the Connect-PSSophosCentral
     Http request format: https://api-{dataRegion}.central.sophos.com/endpoint/v1/endpoints/{endpointId}
     .LINK
     https://developer.sophos.com/docs/endpoint-v1/1/routes/endpoints/%7BendpointId%7D/get
@@ -17,21 +18,6 @@ function Get-PSSophosCentralEndpoint {
     #>
     [CmdletBinding()]
     param (
-        # Data region to be included in the URI of thhe HTTPS request
-        [Parameter(Mandatory = $true)]
-        [string]
-        $dataregion,
-        
-        # TenantID for the header
-        [Parameter(Mandatory = $true)]
-        [string]
-        $tenantID,
-        
-        # Used in the uri request header
-        [Parameter(mandatory = $true)]
-        [string]
-        $token,
-
         # The ID of the device in Sophos Central
         [Parameter(Mandatory = $true)]
         [string]
@@ -53,19 +39,20 @@ function Get-PSSophosCentralEndpoint {
         [switch]
         $Full
     )
-    
+
     begin {
         Write-Verbose "[BEGIN ] Starting: $($MyInvocation.Mycommand)"
         $headers = @{
-            "Authorization" = "Bearer $token"
-            "X-Tenant-ID"   = $TenantID
+            "Authorization" = "Bearer $script:token"
+            "X-Tenant-ID"   = $script:TenantID
         }
         Write-Verbose "[BEGIN ] $headers"
     } #begin
-    
+
     process {
-        $url = "{0}/endpoint/v1/endpoints/{1}" -f $dataregion, $endpointId
+        $url = "{0}/endpoint/v1/endpoints/{1}" -f $script:dataregion, $endpointId
         Write-Verbose "URI: [$url]"
+        Write-Verbose "Dataregion [$($script:dataregion)]"
 
         try {
             Invoke-RestMethod -Uri $url -Method GET -Headers $headers
@@ -74,7 +61,7 @@ function Get-PSSophosCentralEndpoint {
             write-error "Error with request: [$_]"
         } #try/catch
     } #process
-    
+
     end {
         Write-Verbose "[END ] Ending: $($MyInvocation.Mycommand)"
     } #end
